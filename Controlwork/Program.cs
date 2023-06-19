6689c62b5ad677773f4da2f9d1054c21c07b368b
DonDrarione <sunny.sashok@gmail.com>
2023-06-19 22:09:12 +0300

    added new method

--- Controlwork/Program.cs
+++ Controlwork/Program.cs
@@ -4,18 +4,20 @@ int numberofelements = Convert.ToInt32(Console.ReadLine());
 
 
 var A = new string[numberofelements];
-
-
-
-
-for (int i = 0; i < numberofelements; i++)
+Adder(A, numberofelements);
+void Adder(string[] A, int numberofelements)
 {
-    A[i] = Console.ReadLine();
+    for (int i = 0; i < numberofelements; i++)
+    {
+        A[i] = Console.ReadLine();
 
+    }
 }
-var b = new List<string>(); 
+var b = new List<string>();
 for (int i = 0; i < numberofelements; i++)
 {
-if (A[i].Length <= 3) b.Add(A[i]);
+    if (A[i].Length <= 3) b.Add(A[i]);
 }
 Console.WriteLine(string.Join(", ", b));
+
+
