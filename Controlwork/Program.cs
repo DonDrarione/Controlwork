86d37691de88aea87e06dfc3f8d98d66e4dc1dc0
sunny <sunny@DESKTOP-UJ1BUSP>
2023-06-19 21:45:03 +0300

    Добавьте файлы проекта.

---
+++ Controlwork/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("number of elements = ");
+int numberofelements = Convert.ToInt32(Console.ReadLine());
+
+
+
+var A = new string[numberofelements];
+
+
+
+for (int i = 0; i < numberofelements; i++)
+{
+    A[i] = Console.ReadLine();
+
+}
+var b = new List<string>(); 
+for (int i = 0; i < numberofelements; i++)
+{
+if (A[i].Length <= 3) b.Add(A[i]);
+}
+Console.WriteLine(string.Join(", ", b));
